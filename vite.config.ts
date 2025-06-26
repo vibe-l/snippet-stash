a4e1282626e796c59601bdd1155025140a09befe
Vianney Stroebel <628818+vibl@users.noreply.github.com>
2025-06-26 16:04:04 +0200

    -

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
+      '/trpc': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
