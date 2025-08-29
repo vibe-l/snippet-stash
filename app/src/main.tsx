d6947ba419c48500d8a265a760fd3a0141c373a4
Vianney Stroebel <628818+vibl@users.noreply.github.com>
2025-08-29 22:30:23 +0200

    -

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -9,10 +9,11 @@ import { createMutators } from './lib/mutators.js';
 
 // The server URL will be loaded from environment variables.
 const server = import.meta.env.VITE_PUBLIC_ZERO_SERVER;
+console.log('server:', server)
 
 const zero = new Zero({
   userID: "anon",
-  server,
+  server: 'http://localhost:4848',
   schema,
   mutators: createMutators(),
 });
